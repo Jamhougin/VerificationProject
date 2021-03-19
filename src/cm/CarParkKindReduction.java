182eb996c57d06ea65dbb13eaf03d59ec1aede02
Jamhougin <49365074+Jamhougin@users.noreply.github.com>
2021-03-19 13:12:20 +0000

    Sixth Commit, Added class to decide what rate reduction to use. Allowing for additional carparkkind to be added without rate class changes.

---
+++ src/cm/CarParkKindReduction.java
@@ -0,0 +1,30 @@
+package cm;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class CarParkKindReduction {
+    int round = 2;
+    public BigDecimal carParkKindReduction(BigDecimal charge, CarParkKind kind){
+        switch (kind) {
+            case VISITOR -> {
+                VisitorRate visReduction = new VisitorRate();
+                charge = visReduction.reduction(charge).setScale(round, RoundingMode.HALF_UP);
+            }
+            case STUDENT -> {
+                StudentRate stuReduction = new StudentRate();
+                charge = stuReduction.reduction(charge).setScale(round, RoundingMode.HALF_UP);
+            }
+            case STAFF -> {
+                StaffRate staReduction = new StaffRate();
+                charge = staReduction.reduction(charge).setScale(round, RoundingMode.HALF_UP);
+            }
+            case MANAGEMENT -> {
+                ManagementRate manReduction = new ManagementRate();
+                charge = manReduction.reduction(charge).setScale(round, RoundingMode.HALF_UP);
+            }
+        }
+
+        return charge;
+    }
+}
